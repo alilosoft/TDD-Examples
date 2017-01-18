7a86824197405c25427a14eb41ff9d34d32fd0bc
FELLAHI Alilo <fellahi.ali@gmail.com>
2017-01-18 23:51:02 +0100

    Adds test condition 4: string length = 1
    TDD Red: 
    the test fails with the input string of 1 char length.

--- tdd-examples/src/test/java/com/midrar/tutorials/tdd_examples/StringHelperTest.java
+++ tdd-examples/src/test/java/com/midrar/tutorials/tdd_examples/StringHelperTest.java
@@ -5,42 +5,48 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
- * @author alilo
- * 	User Story:
- * 	Given: a string 'str' of n chars;
- * 	When: 'str' length is greater then or equal to 2 chars;
- * 	Then: swap the last tow chars;
+ * @author alilo User Story: Given: a string 'str' of n chars; When: 'str'
+ *         length is greater then or equal to 2 chars; Then: swap the last tow
+ *         chars;
  * 
- *  Conditions:
- *  'AB' => 'BA', 'ABC' => 'ACB', 'A' => 'A', '' => '', null => throws exception
+ *         Conditions: 'AB' => 'BA', 'ABC' => 'ACB', 'A' => 'A', '' => '', null
+ *         => throws exception
  */
 
 public class StringHelperTest {
-	
+
 	StringHelper stringHelper = new StringHelper();
-	
-	// test condition 1: string with 2 chars length 
+
+	// test condition 1: string with 2 chars length
 	@Test
 	public void testStringWith2Chars() {
 		String expected = "BA";
 		String actual = stringHelper.swapLast2Chars("AB");
 		assertEquals(expected, actual);
 	}
-	
-	// test condition 2: string with 3 chars length 
+
+	// test condition 2: string with 3 chars length
 	@Test
 	public void testStringWith3Chars() {
 		String expected = "ACB";
 		String actual = stringHelper.swapLast2Chars("ABC");
 		assertEquals(expected, actual);
 	}
-	
-	// test condition 3: string with n chars length 
-		@Test
-		public void testStringWithNChars() {
-			String expected = "ABCDEFGIH";
-			String actual = stringHelper.swapLast2Chars("ABCDEFGHI");
-			assertEquals(expected, actual);
-		}
+
+	// test condition 3: string with n chars length
+	@Test
+	public void testStringWithNChars() {
+		String expected = "ABCDEFGIH";
+		String actual = stringHelper.swapLast2Chars("ABCDEFGHI");
+		assertEquals(expected, actual);
+	}
+
+	// test condition 4: string with 1 char length
+	@Test
+	public void testStringWith1Char() {
+		String expected = "A";
+		String actual = stringHelper.swapLast2Chars("A");
+		assertEquals(expected, actual);
+	}
 
 }
