5435004a74a374b62cae30329ade0c3470d134f2
FELLAHI Alilo <fellahi.ali@gmail.com>
2017-01-18 21:25:33 +0100

    First Commit
    Create new project for TDD examples with few classes and test cases

---
+++ tdd-examples/src/main/java/com/midrar/tutorials/tdd_examples/App.java
@@ -0,0 +1,13 @@
+package com.midrar.tutorials.tdd_examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tdd-examples/src/test/java/com/midrar/tutorials/tdd_examples/StringHelperTest.java
@@ -0,0 +1,25 @@
+package com.midrar.tutorials.tdd_examples;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author alilo
+ * 	User Story:
+ * 	Given: a string 'str' of n chars;
+ * 	When: 'str' length is greater then or equal to 2 chars;
+ * 	Then: swap the 2 last chars;
+ * 
+ *  Conditions:
+ *  'AB' => 'BA', 'ABC' => 'ACB', 'A' => 'A', '' => ''
+ */
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
