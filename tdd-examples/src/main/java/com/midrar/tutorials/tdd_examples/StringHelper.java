d128300ba96afbc70106c1391a42a26e52d4e5d4
FELLAHI Alilo <fellahi.ali@gmail.com>
2017-01-18 23:55:57 +0100

    Corrects the swapLast2Chars(str) method.
    TDD Green:
    correct the 'swapLast2Chars' method 
    to support string of 1 char length as input.

--- tdd-examples/src/main/java/com/midrar/tutorials/tdd_examples/StringHelper.java
+++ tdd-examples/src/main/java/com/midrar/tutorials/tdd_examples/StringHelper.java
@@ -4,6 +4,8 @@ public class StringHelper {
 
 	public String swapLast2Chars(String str) {
 		int strLenght = str.length();
+		if (strLenght == 1) return str;
+		
 		String strExceptLast2Chars = str.substring(0, strLenght - 2);
 		char secondLastChar = str.charAt(strLenght - 2);
 		char lastChar = str.charAt(strLenght - 1);
