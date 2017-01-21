e4ca5895ddab73454c4816ef3ef2e53cd9f5a5a1
FELLAHI Alilo <fellahi.ali@gmail.com>
2017-01-21 15:12:43 +0100

    Adds Second Test Case

---
+++ tdd-examples/src/test/java/com/midrar/tutorials/tdd_examples/StringHelperSecondTest.java
@@ -0,0 +1,14 @@
+package com.midrar.tutorials.tdd_examples;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperSecondTest {
+
+	@Test
+	public void testSwapLast2Chars() {
+		fail("Not yet implemented");
+	}
+
+}
